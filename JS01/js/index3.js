2f472ff1f5c1bde67bf70862c4a3afb59837d0a7
Hyejin Park <hyejin.park@one-line.com>
2019-01-31 17:21:00 +0900

    박스생성 완료

--- JS01/js/index3.js
+++ JS01/js/index3.js
@@ -2,8 +2,11 @@ const log = console.log;
 
  //반복문
 //  for(초기값; 조건; 증가) {
-for(var i=1; i<=100; i++){
+
+$("#bt-ok").click(function(){
+for(var i=1; i<=100; i++) {
     // log(i);
-    sum = sum + i;
+    // sum = sum + i;
+    $("#box-wrap").append('<div>' +i+ '</div>');
  }
- log(sum;)
+});
